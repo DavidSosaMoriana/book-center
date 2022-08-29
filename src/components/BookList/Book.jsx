6c353c99580934fdff977c8544d8ff4a5b8d51dd
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-29 12:35:45 +0100

    :zap: About page

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -12,10 +12,10 @@ const Book = (book) => {
                 <Link to={`/book/${book.id}`} {...book}>
                     <span>{book.title}</span>
                 </Link>
-
+                
                 <div className="book-item-info-item author fs-15">
                     <span className="text-capitalize fw-7">Author: </span>
-                    <span>{book.author.join(', ')}</span>
+                    <span>{book.author}</span>
                 </div>
 
                 <div className="book-item-info-item edition-count fs-15">
